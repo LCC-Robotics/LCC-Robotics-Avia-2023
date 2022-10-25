ac4996716a7369237dfe3172c17e716ffb888c24
Angela Wang <awang@wearelcc.ca>
2022-10-25 17:39:23 -0400

    second commit

---
+++ main.ino
@@ -0,0 +1,25 @@
+#include <CrcLib.h>
+namespace Crc;
+
+void setup {
+    Serial.begin(9600);
+    CrcLib::Initialize();
+
+    CrcLib::SetDigitalPinMode(CRC_DIG_1, INPUT);
+    CrcLib::SetDigitalPinMode(CRC_DIG_2, INPUT);
+    
+    CrcLib::InitializePwmOutput(CRC_PWM_5);
+    
+
+}
+
+void loop {
+    CrcLib::Update();
+
+    if (CRC_DIG_4 == 1){
+        if (BUTTON::ARROW_UP == 1){
+            CrcLib::SetPwmOutput(CRC_PWM_5, 0);
+        }
+    }
+
+}
