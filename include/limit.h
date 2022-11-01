09355af9a6b80a8563d27f60672837ef145a22a5
Kenneth Lew <73978231+lew1101@users.noreply.github.com>
2022-11-01 11:34:40 -0400

    refactor: safety now uses Range object based on Pair for [min, max]

--- include/limit.h
+++ include/limit.h
@@ -10,21 +10,28 @@
 #define PWM_LIMIT_NEGATIVE -127, 0
 
 /**
+ * @brief Stores a range (min, max)
+ * 
+ * @tparam T number type
+ */
+template <class T = int8_t>
+using Range = Pair<T, T>; 
+
+/**
  * @brief Simple function which prevents robot from committing self die by clamping the raws between min and max when limit switch is activated
  *
  * @tparam T number type
  * @param activated is safety switch activated
  * @param raw raw input
- * @param min inclusive
- * @param max inclusive
+ * @param range [min, max]
  * @return T evaluated output
  */
-template <typename T>
-inline T safety(bool activated, T raw, T min, T max)
+template <class T>
+inline T safety(bool activated, T raw, Range<T> range)
 {
     if (!activated)
         return raw;
-    return constrain(raw, min, max);
+    return constrain(raw, range.first, range.second);
 }
 
 /**
@@ -37,7 +44,7 @@ inline T safety(bool activated, T raw, T min, T max)
  * @param max_change maxmimum allowed deviation of raw from prev_out
  * @return T evaluated output
  */
-template <typename T>
+template <class T>
 inline T limitSlew(T raw, T prev_out, T max_change)
 {
     T delta = raw - prev_out;
