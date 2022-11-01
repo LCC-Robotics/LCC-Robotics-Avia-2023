aef2a851d265b1ad813b0eaeae2be45f9f07f10f
Kenneth Lew <73978231+lew1101@users.noreply.github.com>
2022-11-01 11:33:37 -0400

    feat: struct Pair to store relevant values together

--- include/utils.h
+++ include/utils.h
@@ -1,5 +1,17 @@
 #ifndef __ROBOTICS_LCC_ROBOTICS_22_23_INC_UTILS_H_
 #define __ROBOTICS_LCC_ROBOTICS_22_23_INC_UTILS_H_
 
+/**
+ * @brief Stores two values
+ * 
+ * @tparam T1 type 1
+ * @tparam T2 type 2
+ */
+template <class T1, class T2>
+struct Pair
+{
+    T1 first;
+    T2 second;
+};
 
 #endif // __ROBOTICS_LCC_ROBOTICS_22_23_INC_UTILS_H_
