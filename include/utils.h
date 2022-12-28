6ba35ddedef242366717aa10407130b2718aaf9b
Kenneth Lew <73978231+lew1101@users.noreply.github.com>
2022-12-28 17:52:16 -0500

    refactor: remove Pair class and directly implement Range class

--- include/utils.h
+++ include/utils.h
@@ -1,15 +1,12 @@
 #ifndef LCC_ROBOTICS_22_23_INCLUDE_UTILS_H_
 #define LCC_ROBOTICS_22_23_INCLUDE_UTILS_H_
 
-// Stripped down and simple implementation of std::pair
-template <class T1, class T2>
-struct Pair {
-    T1 first;
-    T2 second;
-};
+#include <math.h>
 
-// Stores a range (min, max)
-template <class T = int>
-using Range = Pair<T, T>;
+template <typename T>
+struct Range {
+    T lower;
+    T upper;
+};
 
 #endif // LCC_ROBOTICS_22_23_INCLUDE_UTILS_H_
