9b037a1ac743e18e617531cb6d1baf4beb266fce
Kenneth Lew <73978231+lew1101@users.noreply.github.com>
2022-12-28 17:52:52 -0500

    feat: implement float version of arduino's map function

--- include/utils.h
+++ include/utils.h
@@ -9,4 +9,10 @@ struct Range {
     T upper;
 };
 
+// implementation of arduino map function using float
+inline float fmap(float v, float in_min, float in_max, float out_min, float out_max)
+{
+    return (v - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 #endif // LCC_ROBOTICS_22_23_INCLUDE_UTILS_H_
