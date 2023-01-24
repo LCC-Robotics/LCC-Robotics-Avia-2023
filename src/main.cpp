eae53a4b7075cd265d00f38f82d9ba9a0a6085e2
Kenneth Lew <73978231+lew1101@users.noreply.github.com>
2023-01-24 13:44:17 -0500

    temporarily comment out automatic elevator code

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,8 @@ constexpr ANALOG FORWARD_CHANNEL = ANALOG::JOYSTICK1_Y;
 constexpr ANALOG YAW_CHANNEL = ANALOG::JOYSTICK1_X;
 
 constexpr ANALOG ELEVATOR_MANUAL_CHANNEL = ANALOG::JOYSTICK2_Y;
-constexpr BUTTON ELEVATOR_NEXT_BUTTON = BUTTON::ARROW_UP;
-constexpr BUTTON ELEVATOR_PREV_BUTTON = BUTTON::ARROW_DOWN;
+// constexpr BUTTON ELEVATOR_NEXT_BUTTON = BUTTON::ARROW_UP;
+// constexpr BUTTON ELEVATOR_PREV_BUTTON = BUTTON::ARROW_DOWN;
 
 // ====================
 // Pins
@@ -28,33 +28,33 @@ constexpr uint8_t LEFT_MOTOR_PIN = CRC_PWM_5;
 constexpr uint8_t RIGHT_MOTOR_PIN = CRC_PWM_6;
 constexpr uint8_t ELEVATOR_MOTOR_PIN = CRC_PWM_7;
 
-constexpr uint8_t ELEVATOR_ENCODER_PIN_A = CRC_DIG_2;
-constexpr uint8_t ELEVATOR_ENCODER_PIN_B = CRC_DIG_3;
+// constexpr uint8_t ELEVATOR_ENCODER_PIN_A = CRC_DIG_2;
+// constexpr uint8_t ELEVATOR_ENCODER_PIN_B = CRC_DIG_3;
 
 // ====================
 // CONSTANTS
 // ====================
 
 constexpr float DEFAULT_SLEW_RATE = 0.75;
-constexpr int ENCODER_STEPS = 30;
+// constexpr int ENCODER_STEPS = 30;
 
-constexpr int ELEVATOR_STAGES = 3;
-constexpr int ELEVATOR_LEVELS = 8;
-constexpr float ELEVATOR_SPOOL_DIAMETER = 2.0; // cm
-constexpr float ELEVATOR_ENCODER_STEP_SIZE = ELEVATOR_SPOOL_DIAMETER * ELEVATOR_STAGES * PI / ENCODER_STEPS; // cm - distance travelled per step of rotary encoder
+// constexpr int ELEVATOR_STAGES = 3;
+// constexpr int ELEVATOR_LEVELS = 8;
+// constexpr float ELEVATOR_SPOOL_DIAMETER = 2.0; // cm
+// constexpr float ELEVATOR_ENCODER_STEP_SIZE = ELEVATOR_SPOOL_DIAMETER * ELEVATOR_STAGES * PI / ENCODER_STEPS; // cm - distance travelled per step of rotary encoder
 
-constexpr Range<long> ELEVATOR_RANGE { 50, 2000 };
-constexpr etl::array<long, ELEVATOR_LEVELS> ELEVATOR_HEIGHTS { 0, 60, 170, 380, 660, 1020, 1460, 1900 }; // mm
+// constexpr Range<long> ELEVATOR_RANGE { 50, 2000 };
+// constexpr etl::array<long, ELEVATOR_LEVELS> ELEVATOR_HEIGHTS { 0, 60, 170, 380, 660, 1020, 1460, 1900 }; // mm
 
 // ====================
 
-PID<long> elevatorPID { 10, 1.0, 1.0, PWM_MOTOR_BOUNDS<long> };
+// PID<long> elevatorPID { 10, 1.0, 1.0, PWM_MOTOR_BOUNDS<long> };
 
 RState lastRemoteState;
 RState remoteState; // custom remote state that uses the forbidden arts
 
-int elevatorLevel = 0;
-long elevatorHeight = 0;
+// int elevatorLevel = 0;
+// long elevatorHeight = 0;
 
 void setup()
 {
@@ -85,11 +85,11 @@ void loop()
         remoteState = RState::Next();
     }
 
-    static etl::debounce<> elevatorNextButton;
-    static etl::debounce<> elevatorPrevButton;
+    // static etl::debounce<> elevatorNextButton;
+    // static etl::debounce<> elevatorPrevButton;
 
-    elevatorNextButton.add(remoteState[ELEVATOR_NEXT_BUTTON]);
-    elevatorPrevButton.add(remoteState[ELEVATOR_PREV_BUTTON]);
+    // elevatorNextButton.add(remoteState[ELEVATOR_NEXT_BUTTON]);
+    // elevatorPrevButton.add(remoteState[ELEVATOR_PREV_BUTTON]);
 
     // max amount that input can deviate by
     const auto maxMotorDeviation = static_cast<int8_t>(DEFAULT_SLEW_RATE * dt);
@@ -100,4 +100,9 @@ void loop()
         limitSlew<int8_t>(remoteState[YAW_CHANNEL], lastRemoteState[YAW_CHANNEL], maxMotorDeviation),
         LEFT_MOTOR_PIN,
         RIGHT_MOTOR_PIN);
+
+    // move elevator
+    CrcLib::SetPwmOutput(
+        ELEVATOR_MOTOR_PIN,
+        limitSlew<int8_t>(remoteState[ELEVATOR_MANUAL_CHANNEL], lastRemoteState[ELEVATOR_MANUAL_CHANNEL], maxMotorDeviation));
 }
