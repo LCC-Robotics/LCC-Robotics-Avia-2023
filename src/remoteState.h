71a84ba8af266725296c0e7497f885a83547956d
Kenneth Lew <73978231+lew1101@users.noreply.github.com>
2023-02-13 14:28:18 -0500

    move definitions of RState into header

--- src/remoteState.h
+++ src/remoteState.h
@@ -6,15 +6,63 @@
 using CrcUtility::ANALOG;
 using CrcUtility::BUTTON;
 
-class RState { // dumb name to avoid confusion with CrcUtility::RemoteState
+using namespace Crc;
+
+class RState { // proxy for CrcUtility::RemoteState
 public:
     RState() = default;
 
-    static RState Next(); // get next remote state
-    static RState Convert(CrcUtility::RemoteState crcRemoteState) noexcept;
+    static inline RState Next()
+    {
+        return Convert(CrcLib::RemoteState());
+    }
+    static RState Convert(CrcUtility::RemoteState crcRemoteState) noexcept
+    {
+        crcRemoteState.joystick1X -= 128;
+        crcRemoteState.joystick1Y -= 128;
+        crcRemoteState.joystick2X -= 128;
+        crcRemoteState.joystick2Y -= 128;
+        crcRemoteState.gachetteG -= 128;
+        crcRemoteState.gachetteD -= 128;
+        return *reinterpret_cast<RState*>(&crcRemoteState); // forbidden c++ dark magic
+    }
 
-    int8_t operator[](ANALOG channel) const noexcept;
-    bool operator[](BUTTON button) const noexcept;
+    int8_t operator[](ANALOG channel) const noexcept
+    {
+        switch (channel) {
+        case ANALOG::JOYSTICK1_X: return joystick1X;
+        case ANALOG::JOYSTICK1_Y: return joystick1Y;
+        case ANALOG::JOYSTICK2_X: return joystick2X;
+        case ANALOG::JOYSTICK2_Y: return joystick2Y;
+        case ANALOG::GACHETTE_R: return gachetteD;
+        case ANALOG::GACHETTE_L: return gachetteG;
+        case ANALOG::NONE:
+        default: return 0;
+        }
+    }
+
+    bool operator[](BUTTON button) const noexcept
+    {
+        switch (button) {
+        case BUTTON::ARROW_RIGHT: return arrowRight;
+        case BUTTON::ARROW_UP: return arrowUp;
+        case BUTTON::ARROW_LEFT: return arrowLeft;
+        case BUTTON::ARROW_DOWN: return arrowDown;
+        case BUTTON::COLORS_RIGHT: return colorRight;
+        case BUTTON::COLORS_UP: return colorUp;
+        case BUTTON::COLORS_LEFT: return colorLeft;
+        case BUTTON::COLORS_DOWN: return colorDown;
+        case BUTTON::L1: return L1;
+        case BUTTON::R1: return R1;
+        case BUTTON::SELECT: return select;
+        case BUTTON::START: return start;
+        case BUTTON::LOGO: return logo;
+        case BUTTON::HATL: return hatL;
+        case BUTTON::HATR: return hatR;
+        case BUTTON::NONE:
+        default: return false;
+        }
+    };
 
 private:
     int8_t joystick1X = 0;
