315c30c2675d4eb8b511562e0b3e92dd44eceb29
Angela Wang <awang@wearelcc.ca>
2023-02-18 00:39:06 -0500

    stuff added at competition

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 
 #include <etl/debounce.h>
 
-#include "motor.h"
-#include "remoteState.h"
+// #include "motor.h"
+// #include "remoteState.h"
 #include "utils.h"
 
 using namespace Crc;
@@ -14,19 +14,21 @@ using namespace Crc;
 // Controller Input
 // ====================
 
-constexpr ANALOG FORWARD_CHANNEL = ANALOG::JOYSTICK1_Y; 
-constexpr ANALOG YAW_CHANNEL = ANALOG::JOYSTICK1_X; 
+constexpr ANALOG FORWARD_CHANNEL = ANALOG::JOYSTICK1_Y;
+constexpr ANALOG YAW_CHANNEL = ANALOG::JOYSTICK1_X;
 
-constexpr ANALOG LINEAR_SLIDE_CHANNEL = ANALOG::JOYSTICK2_Y; 
+constexpr ANALOG LINEAR_SLIDE_CHANNEL = ANALOG::JOYSTICK2_Y;
 
-constexpr BUTTON DIE_BUTTON = BUTTON::LOGO; 
+constexpr ANALOG STOP_IT = ANALOG::GACHETTE_L;
+
+constexpr BUTTON DIE_BUTTON = BUTTON::LOGO;
 constexpr BUTTON FLOOR_FLIPPER_BUTTON = BUTTON::COLORS_DOWN;
 
-constexpr BUTTON EXTENDER_FORWARDS_BUTTON = BUTTON::R1; 
-constexpr BUTTON EXTENDER_BACKWARDS_BUTTON = BUTTON::L1; 
+constexpr BUTTON EXTENDER_FORWARDS_BUTTON = BUTTON::R1;
+constexpr BUTTON EXTENDER_BACKWARDS_BUTTON = BUTTON::L1;
 
-constexpr BUTTON EXTENDER_FLIP_FORWARDS_BUTTON = BUTTON::COLORS_LEFT; 
-constexpr BUTTON EXTENDER_FLIP_BACKWARDS_BUTTON = BUTTON::COLORS_RIGHT; 
+constexpr BUTTON EXTENDER_FLIP_FORWARDS_BUTTON = BUTTON::COLORS_LEFT;
+constexpr BUTTON EXTENDER_FLIP_BACKWARDS_BUTTON = BUTTON::COLORS_RIGHT;
 
 // ====================
 // Pins
@@ -42,36 +44,18 @@ constexpr uint8_t FLOOR_FLIPPER_PIN = CRC_PWM_4;
 constexpr uint8_t FLOOR_FLIPPER_ENCODER_PIN_A = CRC_ENCO_A;
 constexpr uint8_t FLOOR_FLIPPER_ENCODER_PIN_B = CRC_ENCO_B;
 
-constexpr uint8_t EXTENDER_MOTOR_PIN = CRC_PWM_7;
+constexpr uint8_t EXTENDER_MOTOR_PIN = CRC_PWM_5;
 constexpr uint8_t EXTENDER_FLIPPER_PIN = CRC_PWM_8;
 
-// ====================
-// Constants
-// ====================
-
-constexpr int32_t flipperUpper = 1300; 
-constexpr int32_t flipperLower = 0;
 
 // ====================
 // Objects
 // ====================
 
-RState lastRemoteState;
-RState remoteState; // custom remote state that uses the forbidden arts
-
-etl::debounce<> dieButtonDebounce;
-etl::debounce<> FloorButtonDebounce;
-
-/*
-SlewLimitingMotor linearSlide { LINEAR_SLIDE_PIN };
-SlewLimitingMotor floorFlipper { FLOOR_FLIPPER_PIN };
-SlewLimitingMotor extender { EXTENDER_MOTOR_PIN };
-SlewLimitingMotor extenderFlipper { EXTENDER_FLIPPER_PIN };
-*/
 
 Encoder flipperEncoder(FLOOR_FLIPPER_ENCODER_PIN_A, FLOOR_FLIPPER_ENCODER_PIN_B);
 
-//Range<int32_t> flipperRange;
+// Range<int32_t> flipperRange;
 
 // ====================
 
@@ -82,15 +66,13 @@ void setup()
     // Initialize pins
     CrcLib::InitializePwmOutput(LEFT_MOTOR_PIN, false);
     CrcLib::InitializePwmOutput(RIGHT_MOTOR_PIN, true);
-    CrcLib::InitializePwmOutput(LINEAR_SLIDE_PIN, false);
-    CrcLib::InitializePwmOutput(FLOOR_FLIPPER_PIN, false);
-    CrcLib::InitializePwmOutput(EXTENDER_MOTOR_PIN, false);
-    CrcLib::InitializePwmOutput(EXTENDER_MOTOR_PIN, false);
 
     CrcLib::InitializePwmOutput(LINEAR_SLIDE_PIN, false);
     CrcLib::InitializePwmOutput(FLOOR_FLIPPER_PIN, false);
     CrcLib::InitializePwmOutput(EXTENDER_MOTOR_PIN, false);
     CrcLib::InitializePwmOutput(EXTENDER_FLIPPER_PIN, false);
+
+    flipperEncoder.write(0);
 
 #ifdef DEBUG // only start serial if in debug mode (serial can affect performance)
     Serial.begin(BAUD); // macro defined in platformio.ini
@@ -101,14 +83,14 @@ void loop()
 {
     CrcLib::Update();
     // Update remote state
-    lastRemoteState = remoteState;
-    remoteState = RState::Next();
+    // lastRemoteState = remoteState;
+    // remoteState = RState::Next();
 
-    dieButtonDebounce.add(remoteState[DIE_BUTTON]);
-    FloorButtonDebounce.add(remoteState[FLOOR_FLIPPER_BUTTON]);
+    // dieButtonDebounce.add(remoteState[DIE_BUTTON]);
+    // FloorButtonDebounce.add(remoteState[FLOOR_FLIPPER_BUTTON]);
 
     // Check if commands are valid
-    if ((CrcLib::IsCommValid() == false) || (CrcLib::ReadDigitalChannel(DIE_BUTTON) == 1)) // controller not connected, don't run loop
+    if (!CrcLib::IsCommValid() || CrcLib::ReadDigitalChannel(DIE_BUTTON)) // controller not connected, don't run loop
     {
         CrcLib::Update();
 
@@ -116,20 +98,20 @@ void loop()
         CrcLib::SetPwmOutput(FLOOR_FLIPPER_PIN, 0);
         CrcLib::SetPwmOutput(EXTENDER_MOTOR_PIN, 0);
         CrcLib::SetPwmOutput(EXTENDER_FLIPPER_PIN, 0);
-        CrcLib::SetPwmOutput(LEFT_MOTOR_PIN, 0); 
-        CrcLib::SetPwmOutput(RIGHT_MOTOR_PIN, 0);// should fix jerking problem from last year?
+        CrcLib::SetPwmOutput(LEFT_MOTOR_PIN, 0);
+        CrcLib::SetPwmOutput(RIGHT_MOTOR_PIN, 0); // should fix jerking problem from last year?
 
         return;
     }
 
-    //const unsigned int dt = CrcLib::GetDeltaTimeMillis();
-    
-     /*
-    linearSlide.update(dt);
-    floorFlipper.update(dt);
-    extender.update(dt);
-    extenderFlipper.update(dt);
-    */
+    // const unsigned int dt = CrcLib::GetDeltaTimeMillis();
+
+    /*
+   linearSlide.update(dt);
+   floorFlipper.update(dt);
+   extender.update(dt);
+   extenderFlipper.update(dt);
+   */
 
     // ======================
     // MOVEMENT
@@ -138,26 +120,36 @@ void loop()
     // const auto maxMotorChange = static_cast<int8_t>(DEFAULT_SLEW_RATE * dt); // max amount that motor output can deviate by
     CrcLib::MoveArcade(FORWARD_CHANNEL, YAW_CHANNEL, LEFT_MOTOR_PIN, RIGHT_MOTOR_PIN);
 
-
     // ======================
     // Linear Slide
     // ======================
 
-    //linearSlide.set(remoteState[LINEAR_SLIDE_CHANNEL]);
-    int linearSlideOutput = CrcLib::ReadAnalogChannel(LINEAR_SLIDE_CHANNEL);
-    CrcLib::SetPwmOutput(LINEAR_SLIDE_PIN, linearSlideOutput);
+    // linearSlide.set(remoteState[LINEAR_SLIDE_CHANNEL]);
+    int linearOutput = CrcLib::ReadAnalogChannel(LINEAR_SLIDE_CHANNEL);
     
+        if (linearOutput >= 5 || linearOutput <= -5) {
+            CrcLib::SetPwmOutput(LINEAR_SLIDE_PIN, linearOutput);
+        } 
+        //else if (CrcLib::ReadAnalogChannel(STOP_IT) > 0){
+        //    CrcLib::SetPwmOutput(LINEAR_SLIDE_PIN, 1);
+        //}
+        else {
+            CrcLib::SetPwmOutput(LINEAR_SLIDE_PIN, 0);
+        }
+    
+
+
     // ======================
     // EXTENDER
     // ======================
 
     int8_t extenderOutput = 0;
 
-    if (remoteState[EXTENDER_FORWARDS_BUTTON]){
-        extenderOutput = PWM_MOTOR_BOUNDS.upper;
+    if (CrcLib::ReadDigitalChannel(EXTENDER_FORWARDS_BUTTON)) {
+        extenderOutput = 127;
     }
-    if (remoteState[EXTENDER_BACKWARDS_BUTTON]){
-        extenderOutput = PWM_MOTOR_BOUNDS.lower;
+    if (CrcLib::ReadDigitalChannel(EXTENDER_BACKWARDS_BUTTON)) {
+        extenderOutput = -128;
     }
     CrcLib::SetPwmOutput(EXTENDER_MOTOR_PIN, extenderOutput);
 
@@ -167,10 +159,10 @@ void loop()
 
     int8_t extenderFlipperOutput = 0;
 
-    if (remoteState[EXTENDER_FLIP_FORWARDS_BUTTON]) {
-        extenderFlipperOutput = PWM_MOTOR_BOUNDS.upper;
-    }  else if (remoteState[EXTENDER_FLIP_BACKWARDS_BUTTON]) {
-        extenderFlipperOutput = PWM_MOTOR_BOUNDS.lower;
+    if (CrcLib::ReadDigitalChannel(EXTENDER_FLIP_FORWARDS_BUTTON)) {
+        extenderFlipperOutput = 127;
+    } else if (CrcLib::ReadDigitalChannel(EXTENDER_FLIP_BACKWARDS_BUTTON)) {
+        extenderFlipperOutput = -128;
     }
     CrcLib::SetPwmOutput(EXTENDER_FLIPPER_PIN, extenderFlipperOutput);
 
@@ -181,15 +173,11 @@ void loop()
     int32_t flipperEncoderValue = flipperEncoder.read();
     int8_t flipperOutput = 0;
 
-    if (CrcLib::ReadDigitalChannel(FLOOR_FLIPPER_BUTTON) && flipperEncoderValue <= 1300) {
-        flipperOutput = -128;
+    if (CrcLib::ReadDigitalChannel(FLOOR_FLIPPER_BUTTON) && flipperEncoderValue < 1300) {
+        flipperOutput = -64;
+    } else if (flipperEncoderValue > 0 ) {
+        flipperOutput = 64;
     }
-    else if (flipperEncoderValue > 0) {
-        flipperOutput = 127;
-    }
-    else {
-        flipperOutput = 0;
-    }
-    
-    CrcLib::SetPwmOutput(FLOOR_FLIPPER_PIN, flipperOutput); 
+
+    CrcLib::SetPwmOutput(FLOOR_FLIPPER_PIN, flipperOutput);
 }
